7e93504ffe52d99cba137f41223531beb435b672
Renato Angelo <renatoangelo@gmail.com>
2023-12-05 19:43:54 -0300

    T

--- cadastro.php
+++ cadastro.php
@@ -8,8 +8,15 @@ echo "<h1>Aqui estão os dados do formulário</h1>";
 echo "<h3>$nome</h3>";
 echo "<h3>$email</h3>";
 
+$x = 0;
 
-
+while ($x <=10) {
+  echo "<hr>";
+  echo $nome;
+  echo "<br>$email";
+  echo "<BR><BR><BR><BR>";
+  $x++;
+}
 ?>
 
 <h4> Aqui agora entra uma programação para conectar e armazenar os dados no banco de dados</h4>
